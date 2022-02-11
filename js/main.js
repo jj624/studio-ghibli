9c6340a70fedb11aad3b3c4a240ec594b7dc9be2
jj624 <97150106+jj624@users.noreply.github.com>
2022-02-11 16:42:25 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(function(){
+    // 1단계 메뉴에 마우스가 올라가면 depth2가 보임
+    // 마우스가 나가면 depth2 안보임
+    $('#gnb > ul > li').hover(
+        function(){
+            $(this).children().addClass('on');
+        },
+        function(){
+            $(this).children().removeClass('on');
+        }
+    );
+    // 패릴랙스 배경 스크롤
+    $.stellar({
+        horizontalScrolling: false,
+        verticalScrolling: true,
+        verticalOffset: 50,
+    });
+});
