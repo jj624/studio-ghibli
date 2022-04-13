f29e43594375d35b0e81192257390245b0057622
jj624 <97150106+jj624@users.noreply.github.com>
2022-04-13 14:32:17 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
+// main.js
+
 $(function(){
+    $('#toggle').click(
+        function(){
+        $('#gnb > ul').toggleClass('on');
+    })
+
     // 1단계 메뉴에 마우스가 올라가면 depth2가 보임
     // 마우스가 나가면 depth2 안보임
     $('#gnb > ul > li').hover(
@@ -9,10 +16,13 @@ $(function(){
             $(this).children().removeClass('on');
         }
     );
+   
+   
     // 패릴랙스 배경 스크롤
     $.stellar({
         horizontalScrolling: false,
         verticalScrolling: true,
         verticalOffset: 50,
     });
+    
 });
